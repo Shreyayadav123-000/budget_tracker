152bfe1df9f33383cad769276d76735425027780
shreya <shreyayadav7981@gmail.com>
2023-08-26 23:15:46 +0530

    done

---
+++ src/modules/home/index.js
@@ -0,0 +1,45 @@
+import styled from "styled-components"; 
+import { useState, useEffect } from "react";
+import OverviewComponent from "./OverviewComponent";
+import TransactionComponent from "./TransactionComponent";
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  font-family: Montserrat;
+  align-items: center;
+  margin: 30px 0px 10px;
+  width: 360px;
+`;
+const HomeComponent= (props) =>{
+    const [transactions, updateTransaction] =useState([]);
+    const [expense, updateExpense] = useState(0);
+    const [income, updateIncome] = useState(0);
+    const addTransaction = (payload) => {
+        const transactionArray= [...transactions];
+        transactionArray.push(payload);
+        updateTransaction(transactionArray);
+    };
+
+    const calculateBalance = () => {
+        let exp=0;
+        let inc=0;
+        transactions.map((payload) => {
+            payload.type==="EXPENSE" 
+            ? (exp=exp+payload.amount) 
+            : (inc=inc+payload.amount);
+        });
+
+        updateExpense(exp);
+        updateIncome(inc);
+    };
+
+    useEffect(() => calculateBalance(),[transactions]);
+    return (
+        <Container>
+            <OverviewComponent addTransaction={addTransaction} expense={expense} income={income}/>
+            <TransactionComponent transactions={transactions}/>
+        </Container>
+    )
+}
+
+export default HomeComponent
